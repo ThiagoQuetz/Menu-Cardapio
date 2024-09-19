05a23aa0b010a85b591a6dae7b8a5cf16c832639
ThiagoQuetz <lizplanejadosjf@gmail.com>
2024-09-19 13:22:24 -0300

    responsive-concluido

--- js/app.js
+++ js/app.js
@@ -596,7 +596,7 @@ cardapio.metodos = {
 cardapio.templates = {
 
     item: `
-                        <div class="col-3 mb-5 animated fadeInUp">
+                        <div class="col-12 col-ld-3 col-md-3 col-sm-6 mb-5 animated fadeInUp">
                             <div class="card card-item" id="\${id}">
                                 <div class="img-produto">
                                     <img src="\${img}" />
